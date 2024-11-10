82b3c3c27bf2119a975c2fbaa0b5fe3c3d0edfc9
IAbsoluteZeroI <kasperekd7@yandex.ru>
2024-11-10 03:34:37 +0300

    init3

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
 import os
 from typing import List
 from starlette.requests import Request
@@ -33,19 +34,32 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "images": images})
 
 @app.post("/upload/")
-async def upload_images(request: Request, files: List[UploadFile] = File(...)):
+async def upload_images(files: List[UploadFile] = File(...)):
+    filenames = []  # Список для хранения названий файлов
+
     for file in files:
         unique_filename = os.path.basename(file.filename)
+        filenames.append(unique_filename)  # Добавляем название файла в список
         file_path = os.path.join(UPLOAD_DIR, unique_filename)
-        os.makedirs(UPLOAD_DIR, exist_ok=True)  # Создаём директорию, если её нет
-        
-        # Сохраняем файл
+        os.makedirs(UPLOAD_DIR, exist_ok=True)
+
         with open(file_path, "wb") as buffer:
             buffer.write(await file.read())
+
+    # Создаем DataFrame с названиями файлов
+    df = pd.DataFrame(filenames, columns=["Filename"])
+
+    # Сохраняем DataFrame в файл CSV
+    csv_file_path = os.path.join(UPLOAD_DIR, "file_list.csv")
+    df.to_csv(csv_file_path, index=False)
 
 @app.get("/complete/", response_class=HTMLResponse)
 async def complete(request: Request):
     return templates.TemplateResponse(request=request, name="upload_complete.html")
+
+@app.post("/process/")
+async def process():
+    ...
 
 
 @app.post("/delete_all/")
